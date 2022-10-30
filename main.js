8876472af174f51e4486139777015dfe6975783d
Rick Vermeil <rickv85@gmail.com>
2022-10-30 09:06:36 -0600

    Refactor and clean up JS

--- main.js
+++ main.js
@@ -1,21 +1,19 @@
-// Query selectors here
+// Query selectors
 var cookButton = document.querySelector('#lets-cook');
 var sideRadio = document.querySelector('#side');
 var mainRadio = document.querySelector('#main-dish');
 var dessertRadio = document.querySelector('#dessert');
 var potIcon = document.querySelector('.cookpot-img');
-var prompt = document.querySelector('.prompt')
-var suggestion = document.querySelector('.suggestion')
-var favoriteSection = document.querySelector('.favorite-section')
-var favoriteButtons = document.querySelector('.favorite-buttons')
-var favorite = document.querySelector('#favorite')
-var viewFavorites = document.querySelector('#view-favorite')
-var favoriteList = document.querySelector('.favorite-list')
-var goBackButton = document.querySelector('.go-back-button')
-var favoriteDishes = document.querySelector("#favorite-dishes")
+var prompt = document.querySelector('.prompt');
+var suggestion = document.querySelector('.suggestion');
+var favoriteSection = document.querySelector('.favorite-section');
+var favoriteButtons = document.querySelector('.favorite-buttons');
+var favorite = document.querySelector('#favorite');
+var viewFavorites = document.querySelector('#view-favorite');
+var favoriteList = document.querySelector('.favorite-list');
+var goBackButton = document.querySelector('.go-back-button');
+var favoriteDishes = document.querySelector("#favorite-dishes");
 var removeButtons = document.querySelectorAll('.remove-buttons');
-
-
 
 // Food options
 var sides = [
@@ -68,29 +66,24 @@ var desserts = [
     "Eclairs"
 ];
 
-
-
-// Event Listeners go here
-cookButton.addEventListener('click', selectFood)
-favorite.addEventListener('click', addFavorite)
-viewFavorites.addEventListener('click', showFavorites)
-goBackButton.addEventListener('click', goBack)
+// Event Listeners
+cookButton.addEventListener('click', selectFood);
+favorite.addEventListener('click', addFavorite);
+viewFavorites.addEventListener('click', showFavorites);
+goBackButton.addEventListener('click', goBack);
 favoriteDishes.addEventListener('click', removeFavorite);
 
-
 // Global vars
-
 var selectedFood;
 var favoriteFoods = [];
 
 // Functions 
-
 class Favorite {
     constructor(name) {
         this.id = Date.now();
         this.name = name;
     }
-}
+};
 
 function getRndFood(array) {
     var arrayIndex = Math.floor(Math.random() * array.length);
@@ -104,7 +97,6 @@ function displayCourse() {
     favoriteSection.classList.remove("hidden");
 };
 
-
 function selectFood() {
     if (sideRadio.checked == true) {
         selectedFood = getRndFood(sides);
@@ -116,7 +108,7 @@ function selectFood() {
         return
     }
 suggestion.innerText = `${selectedFood}!`;
-displayCourse()
+displayCourse();
 favoriteList.classList.add("hidden");
 };
 
@@ -129,52 +121,36 @@ function addFavorite() {
     }
     favoriteFoods.push(newFood);
     listFavorites();
-}
+};
 
 function showFavorites() {
     prompt.classList.add("hidden");
     suggestion.classList.add("hidden");
     favoriteSection.classList.add("hidden");
     favoriteList.classList.remove("hidden");
-}
-
+};
 
 function listFavorites() {
-    // Needs to reset the HTML every time view favorites is pressed
     favoriteDishes.innerHTML = " ";
     for (let index = 0; index < favoriteFoods.length; index++) {
         favoriteDishes.innerHTML += 
         `<p id="${favoriteFoods[index].id}">${favoriteFoods[index].name}&nbsp</p>
         <button id="${favoriteFoods[index].id}" class="remove-buttons">REMOVE</button>`
     }
-}
-
-
+};
 
 function removeFavorite(event) {
-    // alert(event.target.className)}   
-    // if (event.target.className == ".remove-buttons") {
     for (let index = 0; index < favoriteFoods.length; index++) {
         if ((event.target.id == favoriteFoods[index].id) && (event.target.className == "remove-buttons")) {
             favoriteFoods.splice(index, 1);
         }
     }
-    // }
-    // event.target.parentNode.remove();
     listFavorites();
-}
-
-// function removeArticle(event) {
-//     if (event.target.class == "remove-buttons") {
-//         removeFavorite(event);
-//     }
-// }
-
-
+};
 
 function goBack() {
     prompt.classList.remove("hidden");
     suggestion.classList.remove("hidden");
     favoriteSection.classList.remove("hidden");
     favoriteList.classList.add("hidden");
-}
+};
